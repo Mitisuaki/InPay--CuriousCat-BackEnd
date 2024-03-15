dd873120e3aee87a2b521ae621795263a6563082
jucezar <juliocezar.m.a@gmail.com>
2024-03-15 18:19:22 -0300

    Inserindo annotation ns classe Models/Users.cs

--- Domain/Models/User.cs
+++ Domain/Models/User.cs
@@ -1,17 +1,23 @@
 namespace InPay__CuriousCat_BackEnd.Domain.Models;
 
+using System.ComponentModel.DataAnnotations;
 using InPay__CuriousCat_BackEnd.Domain.Models.Interfaces;
 
 public class User : Entity
 {
-    public int Id { get; set; }
     public string? NickName { get; set; }
     public string? Email { get; set; }
     public string? Password { get; set; }
     public List<int>? Phones { get; set; }
     public IAccount[]? Accounts { get; set; }
+
+
+    [DisplayFormat(DataFormatString	="{0:dd-MM-yyyy}")]
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    [DisplayFormat(DataFormatString	="{0:dd-MM-yyyy}")]
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    
     public bool IsActive { get; set; }
     public bool IsAdmin { get; set; }
     public string? RecoveryCode { get; set; }
