3360b6a61075edcd6d2f89c59221cb35a573eeb0
jucezar <juliocezar.m.a@gmail.com>
2024-03-14 23:09:03 -0300

    Instalacao do pacote EntityFramework - para fazer Migrations

--- Domain/Context/UserContext.cs
+++ Domain/Context/UserContext.cs
@@ -1,12 +1,13 @@
+using InPay__CuriousCat_BackEnd.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace InPay__CuriousCat_BackEnd.Domain;
 
 public class UserDbContext : DbContext {
     
-    public UserDbContext(DbContextOptions<UserDbContext> options) :base(options) {
+    public UserDbContext(DbContextOptions<UserDbContext> options) 
+    :base(options) { }
 
-    }
-
+    public DbSet<User> Users { get; set; }
 
 }
