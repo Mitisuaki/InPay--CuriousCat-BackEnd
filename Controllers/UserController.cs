705eca9836fe85013db1ed43a91bfb3cd2790f8c
Miti <mitisuaki@gmail.com>
2024-03-21 01:31:17 -0300

    .

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
-using AutoMapper;
 using InPay__CuriousCat_BackEnd.Domain.Models;
 using InPay__CuriousCat_BackEnd.Domain.DTOs.User;
+using InPay__CuriousCat_BackEnd.Domain.Services;
 
 
 
@@ -15,8 +15,9 @@ public class UserController() : ControllerBase
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(User))]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<IActionResult> CreateUser(UserCreateDTO user)
+    public async Task<IActionResult> Create(UserCreateDTO user)
     {
+        // var userMapped = UserServices.CreateUser(user);
 
         return Created();
     }
