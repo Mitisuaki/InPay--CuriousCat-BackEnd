d2bd9cd6d80d492fd2a22151823156fc3b0e1907
Miti <mitisuaki@gmail.com>
2024-03-20 22:38:29 -0300

    fixed items that won`t be null

--- Domain/Models/User.cs
+++ Domain/Models/User.cs
@@ -6,12 +6,12 @@ namespace InPay__CuriousCat_BackEnd.Domain.Models;
 
 public class User : IdentityUser
 {
-    public string? NickName { get; set; }
-    public List<int>? Phones { get; set; }
+    public string NickName { get; set; } = null!;
+    public List<int> Phones { get; set; } = null!;
     public IAccount[]? Accounts { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     public bool IsActive { get; set; }
     public bool IsAdmin { get; set; }
-    public string? RecoveryCode { get; set; }
+    public string RecoveryCode { get; set; } = null!;
 }
