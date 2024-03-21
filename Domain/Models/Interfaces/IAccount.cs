9382c10fce5f2928a0c9f4bcec538d6be85cc152
Miti <mitisuaki@gmail.com>
2024-03-21 01:31:00 -0300

    Added an Account class to get the Interface IAccount

--- Domain/Models/Interfaces/IAccount.cs
+++ Domain/Models/Interfaces/IAccount.cs
@@ -11,7 +11,25 @@ public interface IAccount
     public double ConfiguredAccLimit { get; set; }
     public double AvailableLimit { get; set; }
     public double TransactionLimit { get; set; }
-    public AccTransaction? Transactions { get; set; }
+    public AccTransaction[]? Transactions { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public bool IsActive { get; set; }
+    public Adress? AdressHistory { get; set; }
+    public string? AccRecoveryCode { get; set; }
+}
+public class Account : IAccount
+{
+    public int AccNumber { get; set; }
+    public int Agency { get; set; }
+    public string? AccNickName { get; set; }
+    public Card[]? Cards { get; set; }
+    public double Balance { get; set; }
+    public double AccLimit { get; set; }
+    public double ConfiguredAccLimit { get; set; }
+    public double AvailableLimit { get; set; }
+    public double TransactionLimit { get; set; }
+    public AccTransaction[]? Transactions { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
     public bool IsActive { get; set; }
