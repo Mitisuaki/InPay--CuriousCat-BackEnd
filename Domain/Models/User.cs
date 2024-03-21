b3528f789296c2c873d4c0777032cc97239b865a
Miti <mitisuaki@gmail.com>
2024-03-20 23:58:44 -0300

    Fixed some things on User class

--- Domain/Models/User.cs
+++ Domain/Models/User.cs
@@ -9,9 +9,9 @@ public class User : IdentityUser
     public string NickName { get; set; } = null!;
     public List<int> Phones { get; set; } = null!;
     public IAccount[]? Accounts { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
-    public bool IsActive { get; set; }
-    public bool IsAdmin { get; set; }
+    public bool IsActive { get; set; } = true;
+    public bool IsAdmin { get; set; } = false;
     public string RecoveryCode { get; set; } = null!;
 }
